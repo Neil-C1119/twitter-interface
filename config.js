9a300b86f9ce37bab273f9dde80428c1fafab635
Neil C <nckooldude1234@gmail.com>
2017-08-06 22:38:22 -0700

    Config gone

--- config.js
+++
@@ -1,6 +0,0 @@
-﻿module.exports = {
-    consumer_key: 'xUm9hljU1Qh8emUtApUSbEYbE',
-    consumer_secret: 'surO28EiP9dqLG2mkklcSJ1SYY6ioWrhk5MHU0wFONecvOoNJE',
-    access_token: '746248101103443969-cJaDUsVz0AGcADyCm4cGwntnr71Ur1K',
-    access_token_secret: 'gixvriV3Va5ye09E6i72S8Nf4dtnbDSEXiegeAdGBSLyw'
-};
